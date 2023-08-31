69b4d811d53e2efb37e1cf7ad9292c7cf1a07943
harryprayiv <harryprayiv@gmail.com>
2023-08-31 10:39:46 -0400

    intial setup

--- flake.nix
+++ flake.nix
@@ -1,11 +1,33 @@
 {
-  description = "A very basic flake";
+  description = "Servant / Halogen 5 Nix Template";
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    #Generic Stuff
+    nixpkgs.follows = "purs-nix/nixpkgs";
+    systems.url = "github:nix-systems/default";
+    utils.url = "github:ursi/flake-utils";
 
+    # Purescript stuff
+    purs-nix.url = "github:purs-nix/purs-nix/ps-0.15";
+    ps-tools.follows = "purs-nix/ps-tools";
+
+    # Haskell stuff
+    # haskell-nix.url = "github:input-output-hk/haskell.nix";
+
+    # *2Nix
+
+    npmlock2nix = {
+      flake = false;
+      url = "github:nix-community/npmlock2nix";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
   };
 }
